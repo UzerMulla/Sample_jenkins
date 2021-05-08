85c3ce3807db54a67ab3ec6511efe2ad88702365
Uzer <Uzer@DESKTOP-Q1EP7JF>
2021-05-09 00:25:14 +0530

    Push

---
+++ src/test/java/Product/Productnfo.java
@@ -0,0 +1,18 @@
+package Product;
+
+import org.testng.annotations.Test;
+
+public class Productnfo
+{
+   @Test
+   public void CreateProduct()
+   {
+	   System.out.println("Executing the CreateProduct");
+   }
+   
+   @Test
+   public void DeliverProduct()
+   {
+	   System.out.println("Executing the DeliverProduct");
+   }
+}
---
+++ src/test/java/Vendor/VendorInfo.java
@@ -0,0 +1,18 @@
+package Vendor;
+
+import org.testng.annotations.Test;
+
+public class VendorInfo
+{
+   @Test
+   public void VendorIndia()
+   {
+	   System.out.println("Executing the VendorIndia");
+   }
+   
+   @Test
+   public void VendorUSA()
+   {
+	   System.out.println("Executing the VendorUSA");
+   }
+}
